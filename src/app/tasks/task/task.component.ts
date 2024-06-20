4a87d055fca3535fd2dcd078a4bb4998bee4e03c
UsamaAliAhmedKhan <ualik0316@gmail.com>
2024-06-21 00:08:30 +0500

    addedDatePipe

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,13 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Tasks } from './task.model';
 import { CardComponent } from "../../sharedUI/card/card.component";
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector: 'app-task',
     standalone: true,
     templateUrl: './task.component.html',
     styleUrl: './task.component.css',
-    imports: [CardComponent]
+    imports: [CardComponent, DatePipe]
 })
 export class TaskComponent {
 
